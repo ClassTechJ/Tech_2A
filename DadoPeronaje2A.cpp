60a2e93e75b95f92d7793c7f071f28638c2ae874
Alejandro Arias <al3jandrorar@gmail.com>
2024-05-08 08:04:29 -0600

    Create DadoPeronaje2A.cpp
    Creando inicio y explicación del código para tirar dados

---
+++ DadoPeronaje2A.cpp
@@ -0,0 +1,13 @@
+/* El siguiente código simula el tirar un dado dentro de un videojuego 
+  usando un numero aleatorio entre 1 a 6.
+  Dependiendo del resultado se tomara una acción:
+  dado = 5 o dado = 2 -> "El personaje avanzara"
+  dado = 6 o dado = 4 -> "El personaje retrocedera"
+  dado = 1 o dado = 3 -> "El personaje se quedara quieto"*/
+#include <iostream>
+#include <time.h>
+
+int main (){
+  int dado = ;
+  if (dado == 5 || dado == 2)
+}
