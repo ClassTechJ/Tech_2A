b2343202b0fbda57e37e4fcf59870f014ae924b5
Alejandro Arias <al3jandrorar@gmail.com>
2024-05-08 11:01:43 -0600

    Update Dadopersonaje2A.cpp

--- Dadopersonaje2A.cpp
+++ Dadopersonaje2A.cpp
@@ -1,13 +1,24 @@
 /* El siguiente código simula el tirar un dado dentro de un videojuego 
   usando un numero aleatorio entre 1 a 6.
   Dependiendo del resultado se tomara una acción:
-  dado = 5 o dado = 2 -> "El personaje avanzara"
-  dado = 6 o dado = 4 -> "El personaje retrocedera"
-  dado = 1 o dado = 3 -> "El personaje se quedara quieto"*/
+  dado = 5 o dado = 2 -> "El personaje se movera"
+  dado = 3 o dado = 1 -> "El personaje retrocedera"
+  dado = 6 o dado = 4 -> "El personaje se quedara quieto"*/
 #include <iostream>
 #include <time.h>
-
-int main (){
-  int dado = ;
-  if (dado == 5 || dado == 2)
+#include <stdlib.h>
+using namespace std;
+int main(){ 
+    srand(time(NULL));
+    int d = 1 + rand() % 6;
+    cout << "La tirada lanzo un " << d << endl; 
+    if (d == 5 || d == 2){
+        cout << "El personaje se mueve." <<endl;
+    } else if (d == 3 || d == 1) {
+        cout << "El personaje retrocede" << endl;
+    } else {
+        cout << "El personaje se queda quieto" << endl;
+    }
+    system("pause");
+    return 0;
 }
